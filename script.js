768c77d4eddeafd7d5d5aa7b01db8a1162faf5b9
Nate Oseni <nateoseni@gmail.com>
2026-03-11 21:06:10 -0400

    Added javascript for mouse, keyboard, form, focus/blur, and event delegation events and linked it to html

---
+++ script.js
@@ -0,0 +1,37 @@
+const mouseButton = document.getElementById('btn');
+mouseButton.addEventListener("mouseover", () => {
+    mouseButton.style.backgroundColor = "red";
+    mouseButton.style.color = "white";
+});
+
+mouseButton.addEventListener("mouseout", () => {
+    mouseButton.style.backgroundColor = "white";
+    mouseButton.style.color = "black";
+});
+
+const keyInput = document.getElementById('keyboardInput');
+keyInput.addEventListener("keydown", (event) => {
+    console.log(`Key pressed: ${event.key}`);
+});
+
+const blurFocus = document.getElementById('focusInput');
+blurFocus.addEventListener("focus", () => {
+    blurFocus.style.backgroundColor = "lightblue";
+});
+
+blurFocus.addEventListener("blur", () => {
+    blurFocus.style.border = "1px solid magenta";
+});
+
+const submitForm = document.getElementById('myForm');
+submitForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    alert("Form submitted!");
+});
+
+const clickButton = document.getElementById('btnContainer');
+clickButton.addEventListener("click", (event) => {
+    if (event.target.tagName === "BUTTON") {
+        document.getElementById('eventMessage').textContent = `${event.target.textContent} clicked!`; 
+    }
+}); 
